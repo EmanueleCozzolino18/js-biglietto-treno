830fceed74f488766a353dead8285c3cdeb71fb6
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-04-29 00:18:57 +0200

    [UPD] Creation of let variables for recording user data

--- js/myscript.js
+++ js/myscript.js
@@ -8,3 +8,9 @@ const currentCont = currentId.innerHTML;
 // ASSEGNAZIONI VARIABILI COSTANTI PER LA STAMPA
 const result = "Il prezzo del biglietto &egrave; "
 const euroValute = " &euro;"
+
+// ASSRGNAZINI VARIABILI LET PER INTERAZIONE CON I DATI UTENTE
+var distanceKm = parseInt(prompt("Quanti chilometri vuoi percorrere ?"));
+var eta = parseInt(prompt("Quanti anni hai ?"));
+var priceKm = 0.21;
+var ticketSales = priceKm * distanceKm;
