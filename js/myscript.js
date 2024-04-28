8124154250764549e5d6452df72721116f72d89d
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-04-29 00:15:44 +0200

    [UPD] Added more constant variables

--- js/myscript.js
+++ js/myscript.js
@@ -5,3 +5,6 @@
 const currentId = document.getElementById("prezzoFinale");
 const currentCont = currentId.innerHTML;
 
+// ASSEGNAZIONI VARIABILI COSTANTI PER LA STAMPA
+const result = "Il prezzo del biglietto &egrave; "
+const euroValute = " &euro;"
