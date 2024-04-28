e2fcd5f69b6999cf231b08d5acf040c434fca6c6
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-04-29 00:24:50 +0200

    [UPD] Added first condition for minors under 18 years of age

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,6 @@
 /* STRICT MODE */
 "use strict";
 
-// ASSEGNAZIONE VARIABILI COSTANTI PER LA STAMPA IN HTML
-const currentId = document.getElementById("prezzoFinale");
-const currentCont = currentId.innerHTML;
-
 // ASSEGNAZIONI VARIABILI COSTANTI PER LA STAMPA
 const result = "Il prezzo del biglietto &egrave; "
 const euroValute = " &euro;"
@@ -14,3 +10,8 @@ var distanceKm = parseInt(prompt("Quanti chilometri vuoi percorrere ?"));
 var eta = parseInt(prompt("Quanti anni hai ?"));
 var priceKm = 0.21;
 var ticketSales = priceKm * distanceKm;
+
+// PRIMA CONDIZIONE PER I MINORI DI 18 ANNI
+if (eta <= 18) {
+    document.getElementById('prezzoFinale').innerHTML = result + ((ticketSales / 100) * 80) + euroValute;
+}
